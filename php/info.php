3dbc87ed8684d0042172451ec53627818aa5b517
anixer0 <anixer42@gmail.com>
2024-06-21 22:03:29 +0800

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,9 +1,9 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
-$website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
-$consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
+$website_name = "AnimXer";
+$apiLink = "https://animeapi-v8nu.onrender.com/"; // add the api here with the '/' at the end!
+$consumet = "https://animestreaming-api.vercel.app/"; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = "https://animeapi-v8nu.onrender.com/"; // same goes for here
 }
 ?>
